27271008f4eebf84f809b5064d1f119aa6f6cae5
kom-1010 <kom-1010@naver.com>
2021-09-10 22:58:30 +0900

    create Posts, PostsRepository

---
+++ src/test/java/com/sbweb/freelecspringbootwebservice/domain/posts/PostsRepositoryTest.java
@@ -0,0 +1,36 @@
+package com.sbweb.freelecspringbootwebservice.domain.posts;
+
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+
+import java.util.List;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+@SpringBootTest
+public class PostsRepositoryTest {
+
+    @Autowired
+    PostsRepository postsRepository;
+
+    @AfterEach
+    public void cleanup() {
+        postsRepository.deleteAll();
+    }
+
+    @Test
+    public void posts_find(){
+        String title = "test_title";
+        String content = "test_content";
+
+        postsRepository.save(Posts.builder().title(title).content(content).author("abc1234@gmail.com").build());
+
+        List<Posts> postsList = postsRepository.findAll();
+
+        Posts posts = postsList.get(0);
+        assertThat(posts.getTitle()).isEqualTo(title);
+        assertThat(posts.getContent()).isEqualTo(content);
+    }
+}
